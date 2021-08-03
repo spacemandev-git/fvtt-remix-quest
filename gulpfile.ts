59c01e2ea0d406c3d0836d6137eda9ebebeb91d8
Dev Bharel <dev@global.id>
2021-08-02 23:01:40 -0400

    updated gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -10,13 +10,14 @@ gulp.task('compile', () => {
 })
 
 gulp.task('copy', async () => {
-  return new Promise<void>((resolve,reject) => {
+  return new Promise((resolve,reject) => {
     gulp.src('README.md').pipe(gulp.dest("dist/"))
     gulp.src("src/module.json").pipe(gulp.dest('dist/'))
     gulp.src("src/lang/**").pipe(gulp.dest('dist/lang/'))
     gulp.src("src/templates/**").pipe(gulp.dest('dist/templates/'))
     gulp.src("src/styles/**").pipe(gulp.dest('dist/styles/'))
     gulp.src("src/assets/**").pipe(gulp.dest('dist/assets/'))
+    //@ts-ignore
     resolve();
   })
 })
